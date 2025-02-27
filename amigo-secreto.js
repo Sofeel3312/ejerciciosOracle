e4442519794ab9792775fefbb6b2151f6009a8b4
Sofeel3312 <fernanda.fonseca3312@gmail.com>
2025-02-26 22:48:39 -0600

    Update amigo-secreto.js
    Lista de amigos numero 1

--- amigo-secreto.js
+++ amigo-secreto.js
@@ -1,1 +1,17 @@
+function agregarAmigos () {
+  //Obtenemos el valor del campo 
+  let agregarAmigosTexto = document.getElementById("Amigo").value;
 
+  //Validamos que el valor no sea vacío
+  if(agregarAmigosTexto === "") {
+    alert('Por favor, ingrese una tarea');
+    return;
+  }
+
+  //Crear elemento en la lista
+  let amigosLista = document.creatElement("li");
+
+  amigosLista.textContent = agregarAmigosTexto + " ";
+
+  
+  
