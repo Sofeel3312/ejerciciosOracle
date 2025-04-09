c28bc3d76b4b5c65320373e836464b7a79fdb1a8
Fernanda Fonseca <fernanda.fonseca3312@gmail.com>
2025-04-08 18:19:54 -0600

    Inicializando en IntelliJ versiones en git

---
+++ src/Ejercicios.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+//public class Ejercicios {
+//    public static void main(String[] args) {
+//        double temperaturaEnCelsius = 30.4;
+//        double temperaturaEnFahrenheit = (temperaturaEnCelsius * 1.8) + 32;
+//
+//        String mensaje = String.format("La temperatura de %f Celsius es equivalente a %f Fahrenheit", temperaturaEnCelsius, temperaturaEnFahrenheit);
+//
+//        System.out.println(mensaje);
+//
+//        int temperaturaEnFahrenheitEntero = (int) temperaturaEnFahrenheit;
+//        System.out.println("La temperatura en Fahrenheit entera es: " + temperaturaEnFahrenheitEntero);
+//        }
+//    }
+//                Scanner scanner = new Scanner(System.in);
+//
+//                System.out.print("Digite su nombre: ");
+//                String nombre = scanner.nextLine();
+//                System.out.print("Digite su edad: ");
+//                int edad = scanner.nextInt();
+//                System.out.print("Digite el monto que deseas invertir este mes: ");
+//                double valor = scanner.nextDouble();
+//
+//                System.out.println(nombre + " que tiene " + edad + " años, irá invertir $ " + valor + " este mes.");
+//
+//                scanner.close();
+//            }
+//        }
+//        switch (expresion) {
+//            case valor1:
+//                // código a ejecutar si la expresión es igual a valor1
+//                break;
+//            case valor2:
+//                // código a ejecutar si la expresión es igual a valor2
+//                break;
+//            // otros casos posibles...
+//            default:
+//                // código a ejecutar si ninguno de los casos anteriores se cumple
+//        }
+//    }
+//}
+//        int dia = 3;
+//        switch (dia) {
+//            case 1:
+//                System.out.println("El día 1 es lunes");
+//                break;
+//            case 2:
+//                System.out.println("El día 2 es martes");
+//                break;
+//            case 3:
+//                System.out.println("El día 3 es miércoles");
+//                break;
+//            // otros casos posibles...
+//            default:
+//                System.out.println("Día no válido");
+//        }
+//    }
+//}
